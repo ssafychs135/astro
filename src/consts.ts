4532c3647fac51a3f5f1196cb173f63ed787ea68
최형선 <sunnychoi135@naver.com>
2026-04-01 11:30:14 +0900

    feat: update navigation and site metadata

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Dev Portfolio & Study Logs";
+export const SITE_DESCRIPTION =
+  "포트폴리오와 개발 학습 기록을 담은 블로그입니다.";
